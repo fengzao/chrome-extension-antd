dc6611fa93ce42540dd41839770f468ea80851b8
fengzao <zhangming360@gmail.com>
2023-12-09 15:52:52 +0800

    演示使用 content script 在頁面注入新 元素

--- content/index.js
+++ content/index.js
@@ -1,24 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
+const App = () => {
+
+    console.log("----------- App ------------")
+
+    return (
+        <div style={{
+            bottom: 100,
+            right: 30,
+            background: "#ccc",
+            width: 40,
+            height: 40,
+            textAlign: 'center',
+            borderRadius: 40,
+            position: "fixed"
+        }}>
+            <div style={{ fontSize: 11, marginTop: "10px" }}>
+                antd
+            </div>
+        </div>
+
+    )
+}
+
+
+
 const div = document.createElement("div")
 div.id = "chrome-extension-div"
 document.body.appendChild(div)
-
 const root = ReactDOM.createRoot(div);
 root.render(
     <React.StrictMode>
-        <div style={{
-            position: "fixed",
-            bottom: 100,
-            right: 100,
-            background: "#ccc",
-            width: 200,
-            height: 100,
-            textAlign: 'center',
-            color:"#111"
-        }}>
-            chrome extensions content js  with react
-        </div>
+        <App />
     </React.StrictMode>
 );
